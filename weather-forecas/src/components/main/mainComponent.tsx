243413162506e01d47158df4fefa606f083a7974
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2023-10-20 20:57:24 +0300

    add heandler loading and error in main compomnent

--- weather-forecas/src/components/main/mainComponent.tsx
+++ weather-forecas/src/components/main/mainComponent.tsx
@@ -1,19 +1,33 @@
+import { useSelector } from "react-redux";
 import { HourlyForecast } from "../hourlyForecast/hourlyForecast";
 import { SearchLocation } from "../searchLocation/searchLocation";
 import { TodaysHightlights } from "../todaysHiglights/todaysHighlights";
 
 import { MainContainer } from "./mainComponent.Styled";
+import { CircularProgress } from "@mui/material";
+import { StoreType } from "../../redux/store";
 
 export const Main = () => {
+  const { loading, error } = useSelector(
+    (state: StoreType) => state.daysForecastReducer
+  );
   return (
-    <MainContainer>
-      <div>
-        <TodaysHightlights />
-        <HourlyForecast />
-      </div>
-      <div>
-        <SearchLocation />
-      </div>
-    </MainContainer>
+    <>
+      {
+        loading ? (
+          <CircularProgress />
+        ) : error ? (
+          <p>Error: {error}</p>
+        ) : (<MainContainer>
+          <div>
+            <TodaysHightlights />
+            <HourlyForecast />
+          </div>
+          <div>
+            <SearchLocation />
+          </div>
+        </MainContainer>)
+      }
+    </>
   );
 };
