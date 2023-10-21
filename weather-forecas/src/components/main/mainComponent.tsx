6a1730e5805404202fcda9f366d990f1e77a63e2
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2023-10-21 12:26:48 +0300

    Revert "add heandler loading and error in main compomnent"
    This reverts commit 243413162506e01d47158df4fefa606f083a7974.

--- weather-forecas/src/components/main/mainComponent.tsx
+++ weather-forecas/src/components/main/mainComponent.tsx
@@ -1,33 +1,19 @@
-import { useSelector } from "react-redux";
 import { HourlyForecast } from "../hourlyForecast/hourlyForecast";
 import { SearchLocation } from "../searchLocation/searchLocation";
 import { TodaysHightlights } from "../todaysHiglights/todaysHighlights";
 
 import { MainContainer } from "./mainComponent.Styled";
-import { CircularProgress } from "@mui/material";
-import { StoreType } from "../../redux/store";
 
 export const Main = () => {
-  const { loading, error } = useSelector(
-    (state: StoreType) => state.daysForecastReducer
-  );
   return (
-    <>
-      {
-        loading ? (
-          <CircularProgress />
-        ) : error ? (
-          <p>Error: {error}</p>
-        ) : (<MainContainer>
-          <div>
-            <TodaysHightlights />
-            <HourlyForecast />
-          </div>
-          <div>
-            <SearchLocation />
-          </div>
-        </MainContainer>)
-      }
-    </>
+    <MainContainer>
+      <div>
+        <TodaysHightlights />
+        <HourlyForecast />
+      </div>
+      <div>
+        <SearchLocation />
+      </div>
+    </MainContainer>
   );
 };
