08cda1a1b21b4916108d119f122d53e32aac2c3c
Denis Markin <denis.markin.95@gmail.com>
2023-10-25 17:45:12 +0400

    added global loading

--- weather-forecas/src/components/general/generalStyled.ts
+++ weather-forecas/src/components/general/generalStyled.ts
@@ -1,7 +1,6 @@
 import { styled } from "styled-components";
 import { Theme, ThemeType } from "../../theme/theme";
 
-
 interface StyledInterface {
   themeStyles: Theme;
   themeType: ThemeType;
@@ -12,4 +11,11 @@ export const GeneralContainer = styled.div<StyledInterface>`
   background-size: cover;
   width: 100%;
   height: 100vh;
+  position: relative;
+`;
+
+export const LoaderDiv = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 40%;
 `;
