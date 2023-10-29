698b1350a7d16b9c9d70cfdbedcb79a7090149e2
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2023-10-29 10:33:17 +0200

    fixed height for general container

--- src/components/general/generalStyled.ts
+++ src/components/general/generalStyled.ts
@@ -11,6 +11,7 @@ export const GeneralContainer = styled.div<StyledInterface>`
   background-size: cover;
   width: 100%;
   height: 100%;
+  min-height: 100vh;
   position: relative;
   @media (max-width: 656px) {
    display: block;
